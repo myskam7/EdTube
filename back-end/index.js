ad4963ec42dd8ee17bf9129bb13a9a5393e65d55
apple <apple@apples-MacBook-Pro.local>
2021-06-11 21:36:09 -0700

    backend - basic api

---
+++ back-end/index.js
@@ -0,0 +1,37 @@
+/*
+* TODO:
+*  - create dataset method that takes keywords from user's search as params.
+*  use the param to find a match, then take all the values and add them to keyword string
+*  -
+*
+*
+*
+*
+*
+* */
+
+
+require('dotenv').config()
+
+const {google} = require('googleapis');
+
+const youtube = google.youtube('v3');
+
+youtube.search.list({
+     key: process.env.YOUTUBE_KEY,
+    part: 'snippet',
+    keyword: 'coding',
+    videoCategoryId: 27,
+    type: 'video',
+    relatedToVideoId: "_uQrJ0TkZlc",
+    q: userSearch
+
+
+
+} ).then(res => {
+    console.log(res.data.items);
+}).catch(err => console.log(err));
+
+
+
+
